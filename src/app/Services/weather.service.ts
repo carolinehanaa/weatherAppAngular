bfac104dbc11b41776e84e4e4cd8764d4477585b
carolinehanaa <carolinehana28@gmail.com>
2023-06-06 15:36:22 -0700

    init

---
+++ src/app/Services/weather.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  // getWeatherData(cityName: string): Observable<WeatherData>{
+  //   return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
+  //     headers: new HttpHeaders()
+  //     .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+  //     .set(environment.XRapidAPIKeyHeaderName, environment.RapidAPIKeyHeaderValue),
+  //     params: new HttpParams()
+  //     .set('city', cityName)
+  //     // .set('units', 'metric')
+  //     // .set('mode', 'json')
+
+  //   })
+  // }
+
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    const apiUrl = `${environment.weatherApiBaseUrl}/city/${cityName}`;
+  
+    const headers = new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName, environment.RapidAPIKeyHeaderValue);
+  
+    return this.http.get<WeatherData>(apiUrl, { headers });
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    RapidAPIKeyHeaderValue: ' 55133102f7msh835a32957847b6ep1bc41bjsn6c54aa172ecb'
+
+
+
+};
